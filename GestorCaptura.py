ffde9b091e170bd301dee642fd96a7ab89a6dba5
kaiziferr <steven.bernal@correo.icesi.edu.co>
2020-06-25 22:22:49 -0500

    first commit

---
+++ GestorCaptura.py
@@ -0,0 +1,24 @@
+import webbrowser as web
+import subprocess as sub
+import time as tm
+
+def readFile():
+	file = open('url_Benignas.txt', 'r')
+	while(True):
+		line = file.readline()
+		RunProcess(line)
+		if not line:
+			break;
+	file.close()
+	
+
+def RunProcess(line):		
+	sub.Popen(['C:/Program Files/Mozilla Firefox/firefox.exe',line])
+	tm.sleep(30)
+	sub.Popen("TASKKIll /im firefox.exe ", shell=True)
+
+	
+
+if __name__ == '__main__':
+	readFile();
+
